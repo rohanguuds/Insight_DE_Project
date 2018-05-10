dd4eed5a6d200b892b36f47befe1093819736e0e
rohanguuds <33246937+rohanguuds@users.noreply.github.com>
2018-05-10 10:56:25 -0700

    Create cassandra_table.py

---
+++ Cassandra/cassandra_table.py
@@ -0,0 +1,11 @@
+#######################################################
+# This script is for creating a table in cassandra    #
+#######################################################
+
+from cassandra.cluster import Cluster
+
+
+cassandra_cluster = Cluster(['54.71.115.97','35.166.209.97','35.166.89.248','34.218.167.77'])
+cassandra_session = cassandra_cluster.connect('oilwell')
+cassandra_session.execute('DROP TABLE IF EXISTS oil_production;')
+cassandra_session.execute('CREATE TABLE oil_production (dt date, well_name text, oil_bbl float, water_bbl float, gas_mscf float, pressure float, watercut float, PRIMARY KEY (dt, well_name));')
