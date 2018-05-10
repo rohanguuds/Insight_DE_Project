6d435f0c3e471622cdc4585bd568721b0b45421c
rohanguuds <33246937+rohanguuds@users.noreply.github.com>
2018-05-10 11:14:14 -0700

    Create kafka_producer.py

---
+++ Kafka/kafka_producer.py
@@ -0,0 +1,21 @@
+import os
+from kafka import KafkaProducer
+import json
+import config
+
+
+def main():
+    producer = KafkaProducer(bootstrap_servers=config.KAFKA_BROKERS)
+    mypath = os.path.dirname(os.path.abspath(__file__))
+    files = os.listdir(mypath + '/../data/')
+    for f in files:
+        with open(mypath + '/../data/' + f) as json_data:
+                ad_data = json.load(json_data)
+        for each in ad_data:
+                msg = json.dumps(each, encoding = 'utf-8')
+                producer.send(config.KAFKA_TOPIC, msg)
+    producer.flush()
+
+
+if __name__ == '__main__':
+    main()
