1745c1479adbebf03c8cabbc31b87613207169e6
rohanguuds <33246937+rohanguuds@users.noreply.github.com>
2018-05-24 10:48:13 -0700

    Update createKafkaTopic.sh

--- Kafka/createKafkaTopic.sh
+++ Kafka/createKafkaTopic.sh
@@ -1,1 +1,5 @@
+# For Batch 
 /usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 2 --topic welldata
+
+# For Streaming
+/usr/local/kafka/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 3 --partitions 2 --topic rtpressure
