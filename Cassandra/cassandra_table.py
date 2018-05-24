26e55aaf2f8421409711b5678cee5847dcbefa91
rohanguuds <33246937+rohanguuds@users.noreply.github.com>
2018-05-24 14:44:45 -0700

    Update cassandra_table.py

--- Cassandra/cassandra_table.py
+++ Cassandra/cassandra_table.py
@@ -8,4 +8,8 @@ from cassandra.cluster import Cluster
 cassandra_cluster = Cluster(['54.71.115.97','35.166.209.97','35.166.89.248','34.218.167.77'])
 cassandra_session = cassandra_cluster.connect('oilwell')
 cassandra_session.execute('DROP TABLE IF EXISTS oil_production;')
-CREATE TABLE oil_production (dt date, well_name text, oil_bbl float, water_bbl float, gas_mscf float, pressure float, PRIMARY KEY (well_name,dt)) WITH CLUSTERING ORDER BY (dt ASC);
+cassandra_session.execute('CREATE TABLE oil_production (dt date, well_name text, oil_bbl float, water_bbl float, gas_mscf float, pressure float, PRIMARY KEY (well_name,dt)) WITH CLUSTERING ORDER BY (dt ASC);')
+cassandra_session.execute('DROP TABLE IF EXISTS monthly_oil;')
+cassandra_session.execute('CREATE TABLE monthly_oil (well_name text, month text, oil_avg float, PRIMARY KEY (well_name, month)) WITH CLUSTERING ORDER BY (month DESC);')
+cassandra_session.execute('DROP TABLE IF EXISTS well_pressure;')
+cassandra_session.execute('CREATE table well_pressure (id int, dt text, pressure_1 int, pressure_2 int, pressure_3 int, pressure_4 int, PRIMARY KEY(dt, id))WITH CLUSTERING ORDER BY (id ASC);')
