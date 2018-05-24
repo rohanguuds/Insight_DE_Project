191a805097e8fe1916229fc51ac3c75e6f7d3b11
rohanguuds <33246937+rohanguuds@users.noreply.github.com>
2018-05-24 11:38:00 -0700

    Create run_kafka_producer_stream.sh

---
+++ Kafka/run_kafka_producer_stream.sh
@@ -0,0 +1,6 @@
+#######################################################
+# This is the bash script to run kafka_producer_stream.py    #
+#######################################################
+
+
+python ./kafka/kafka_producer_stream.py
